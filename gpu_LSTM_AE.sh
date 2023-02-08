9666bd7c7e425dd3d30e0686efa9f9cb926c3c22
Alberto Bassi <alberto.bassi.3@studenti.unipd.it>
2023-02-08 13:19:27 +0100

    up

--- gpu_LSTM_AE.sh
+++ gpu_LSTM_AE.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=gpu_LSTM_AE       # create a short name for your job
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --gres=gpumem:4G                 # memory per gpu-core (4G is default)
 #SBATCH --time=120:00:00          # total run time limit (HH:MM:SS)
