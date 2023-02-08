2821fc3eeb3352e9dee353874b3365d074aacf77
Alberto Bassi <alberto.bassi.3@studenti.unipd.it>
2023-02-08 17:41:37 +0100

    modified sbatch files

--- gpu_LSTM.sh
+++ gpu_LSTM.sh
@@ -1,19 +1,16 @@
-#!/bin/bash
-#SBATCH --job-name=gpu_LSTM       # create a short name for your job
+#!/bin/bash -l
+
+#SBATCH --job-name=gpu_LSTM       
+#SBATCH --time=24:00:00          
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --gres=gpu:1        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --gres=gpumem:4G                 # memory per gpu-core (4G is default)
-#SBATCH --time=24:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --cpus-per-task=24
+#SBATCH --constraint=gpu
+#SBATCH --account=em09
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END,FAIL     
 #SBATCH --output=gpu_LSTM.out
 #SBATCH --error=gpu_LSTM.err
-#SBATCH --account=em09
-#SBATCH --constraint=gpu
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-
 module load daint-gpu PyTorch      
-python3 src/MultiBasinHydro_lupoalberto98/LSTM_main.py          # Execute the program
+python3 src/MultiBasinHydro_lupoalberto98/LSTM_main.py        
