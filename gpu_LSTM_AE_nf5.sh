56114f1c07d3c26b0bbbe61fc9ec890a764f6e93
lupoalberto98 <alberto.bassi98@gmail.com>
2023-02-09 18:17:09 +0100

    up

--- gpu_LSTM_AE_nf5.sh
+++ gpu_LSTM_AE_nf5.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
-#SBATCH -J cpu_LSTM_AE
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32            # 8 cores
-#SBATCH --time=120:00:00                   # 120-hours run-time
-#SBATCH --mem-per-cpu=1G            # 1  GB per core
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END,FAIL
-#SBATCH --output=cpu_LSTM_AE.out
-#SBATCH --error=cpu_LSTM_AE.err
+#!/bin/bash -l
 
-module load gcc/8.2.0 python_gpu/3.10.4    # Load modules      
-python3 src/MultiBasinHydro_lupoalberto98/LSTM_AE_nf5_main.py          # Execute the program
+#SBATCH --job-name=gpu_LSTM_AE_nf5      
+#SBATCH --time=24:00:00 
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=24
+#SBATCH --constraint=gpu
+#SBATCH --account=em09      
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END,FAIL     
+#SBATCH --output=gpu_LSTM_AE_nf5.out
+#SBATCH --error=gpu_LSTM_AE_nf5.err
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+module load daint-gpu PyTorch     
+python3 src/MultiBasinHydro_lupoalberto98/LSTM_AE_nf5_main.py         
