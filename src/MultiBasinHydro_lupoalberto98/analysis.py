18ea735daa1abd88122a44c3839f534b9d7ace0c
lupoalberto98 <alberto.bassi98@gmail.com>
2023-02-09 11:54:57 +0100

    modified analysis

--- src/MultiBasinHydro_lupoalberto98/analysis.py
+++ src/MultiBasinHydro_lupoalberto98/analysis.py
@@ -43,43 +43,50 @@ if __name__ == '__main__':
     print("Number of basins: %d" %num_basins)
     print("Number of points: %d" %seq_len)
     """
-    dirpath="checkpoints/lstm-ae/"
-    val_loss = []
+    dir_ae="checkpoints/lstm-ae/"
+    dir_lstm="checkpoints/lstm/"
+    ae_nse = []
+    lstm_nse = []
     epochs = []
     for i in range(21):
         epoch = str(i*10 +9).rjust(2,"0") 
-        filename="hydro-lstm-ae-epoch="+epoch+".ckpt"
-        path = os.path.join(dirpath, filename)
+        file_ae="hydro-lstm-ae-epoch="+epoch+".ckpt"
+        path_ae = os.path.join(dir_ae, file_ae)
+        file_lstm="hydro-lstm-epoch="+epoch+".ckpt"
+        path_lstm = os.path.join(dir_lstm, file_lstm)
         
         # retrieve validation loss for plotting
-        checkpoint = torch.load(path, map_location=lambda storage, loc: storage)
-        val_loss.append(-checkpoint["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
+        checkpoint_ae = torch.load(path_ae, map_location=lambda storage, loc: storage)
+        ae_nse.append(-checkpoint_ae["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
+        checkpoint_lstm = torch.load(path_lstm, map_location=lambda storage, loc: storage)
+        lstm_nse.append(-checkpoint_lstm["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
         epochs.append(float(epoch))
 
-        # plot weights of linear feature space
-        model = Hydro_LSTM_AE.load_from_checkpoint(path)
-        enc_liner_2 = model.encoder.encoder_lin[4].weight # select last linear encoder layer
-        fig, axs = plt.subplots(9,3, figsize=(18,6), sharex=True)
-        for j in range(3):
-            for i in range(9):
-                ax = axs[i,j]
-                feature = j*9 + i 
-                ax.set_xlabel(str(feature))
-                ax.hist(enc_liner_2.detach().numpy()[feature,:], density=True)
-        path_hist = os.path.join("hist/","hist-epoch="+epoch+".png")
-        fig.savefig(path_hist)
+        # # plot weights of linear feature space
+        # model = Hydro_LSTM_AE.load_from_checkpoint(path)
+        # enc_liner_2 = model.encoder.encoder_lin[4].weight # select last linear encoder layer
+        # fig, axs = plt.subplots(9,3, figsize=(18,6), sharex=True)
+        # for j in range(3):
+        #     for i in range(9):
+        #         ax = axs[i,j]
+        #         feature = j*9 + i 
+        #         ax.set_xlabel(str(feature))
+        #         ax.hist(enc_liner_2.detach().numpy()[feature,:], density=True)
+        # path_hist = os.path.join("hist/","hist-epoch="+epoch+".png")
+        # fig.savefig(path_hist)
         
     fig2, ax2 = plt.subplots(1,1,figsize=(10,10))
-    ax2.plot(epochs,val_loss)
+    ax2.plot(epochs,ae_nse, label="LSTM-AE")
+    ax2.plot(epochs,lstm_nse, label="LSTM")
     ax2.set_xlabel("epoch")
     ax2.set_ylabel("NSE")
     fig2.savefig("hydro-lstm-ae_NSE.png")
     
-    # find maximum
-    index = np.argmax(val_loss)
-    print(index)
-    max_epoch = epochs[index]
-    print("Best model attained at epoch: %d" %max_epoch)
-    print("Best model NSE: %d"%-val_loss[index])
+    # # find maximum
+    # index = np.argmax(val_loss)
+    # print(index)
+    # max_epoch = epochs[index]
+    # print("Best model attained at epoch: %d" %max_epoch)
+    # print("Best model NSE: %d"%-val_loss[index])
  
    
