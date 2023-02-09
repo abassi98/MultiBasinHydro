e2173c694458432454578249326779239496c9ef
lupoalberto98 <alberto.bassi98@gmail.com>
2023-02-09 18:50:11 +0100

    modified analysis-loss

--- src/MultiBasinHydro_lupoalberto98/analysis_loss.py
+++ src/MultiBasinHydro_lupoalberto98/analysis_loss.py
@@ -47,17 +47,26 @@ if __name__ == '__main__':
     print("Number of points: %d" %seq_len)
     """
     data_ae = glob.glob("checkpoints/lstm-ae/*.ckpt")
+    data_ae_nf5 = glob.glob("checkpoints/lstm-ae-nf5/*.ckpt")
     data_lstm = glob.glob("checkpoints/lstm/*.ckpt")
     ae_nse = []
+    ae_nf5_nse = []
     lstm_nse = []
     epochs_ae = []
+    epochs_ae_nf5 = []
     epochs_lstm = []
     for file in data_ae:
         epoch = re.findall(r'\b\d+\b', file)
         epochs_ae.append(int(epoch[0]))
         checkpoint_ae = torch.load(file, map_location=lambda storage, loc: storage)
         ae_nse.append(-checkpoint_ae["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
-
+    
+    for file in data_ae_nf5:
+        epoch = re.findall(r'\b\d+\b', file)
+        epochs_ae_nf5.append(int(epoch[0]))
+        checkpoint_ae_nf5 = torch.load(file, map_location=lambda storage, loc: storage)
+        ae_nf5_nse.append(-checkpoint_ae_nf5["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
+    
     for file in data_lstm:
         epoch = re.findall(r'\b\d+\b', file)
         epochs_lstm.append(int(epoch[0]))
@@ -67,8 +76,10 @@ if __name__ == '__main__':
     fig2, ax2 = plt.subplots(1,1,figsize=(10,10))
     epochs_ae, ae_nse = zip(*sorted(zip(epochs_ae, ae_nse)))
     epochs_lstm, lstm_nse = zip(*sorted(zip(epochs_lstm, lstm_nse)))
-    ax2.plot(epochs_ae,ae_nse, label="LSTM-AE")
+    epochs_ae_nf5, lstm_ae_nf5 = zip(*sorted(zip(epochs_ae_nf5, ae_nf5_nse)))
+    ax2.plot(epochs_ae,ae_nse, label="LSTM-AE-27F")
     ax2.plot(epochs_lstm,lstm_nse, label="LSTM")
+    ax2.plot(epochs_ae_nf5,ae_nf5_nse, label="LSTM-AE-5F")
     ax2.set_xlabel("epoch")
     ax2.set_ylabel("NSE")
     ax2.legend()
