9fd0ce6e48a68be8a5cd4d63787301b8612bce49
lupoalberto98 <alberto.bassi98@gmail.com>
2023-02-09 12:40:41 +0100

    analysis job

---
+++ job_analysis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=analysis       
+#SBATCH --time=24:00:00          
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=24
+#SBATCH --account=em09
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END,FAIL     
+#SBATCH --output=analsyis .out
+#SBATCH --error=analsyis .err
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+module load daint-gpu PyTorch      
+python3 src/MultiBasinHydro_lupoalberto98/analysis.py        
