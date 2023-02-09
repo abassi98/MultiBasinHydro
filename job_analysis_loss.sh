8b54c821fde68f5cfb00186ab763b558aeee2179
lupoalberto98 <alberto.bassi98@gmail.com>
2023-02-09 18:06:31 +0100

    renamed analysis

--- job_analysis_loss.sh
+++ job_analysis_loss.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=analysis       
+#SBATCH --job-name=analysis_loss       
 #SBATCH --time=24:00:00          
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=24
 #SBATCH --account=em09
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END,FAIL     
-#SBATCH --output=analsyis.out
-#SBATCH --error=analsyis.err
+#SBATCH --output=analsyis_loss.out
+#SBATCH --error=analsyis_loss.err
 #SBATCH --constraint=mc
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 module load daint-gpu PyTorch      
-python3 src/MultiBasinHydro_lupoalberto98/analysis.py        
+python3 src/MultiBasinHydro_lupoalberto98/analysis_loss.py        
