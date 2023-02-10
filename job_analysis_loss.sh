9289052ec4e51e110a3927a466272845b24408f7
Alberto Bassi <alberto.bassi.3@studenti.unipd.it>
2023-02-10 09:45:01 +0100

    up!

--- job_analysis_loss.sh
+++ job_analysis_loss.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=analysis_loss       
 #SBATCH --time=24:00:00          
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=8
 #SBATCH --account=em09
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END,FAIL     
--- src/MultiBasinHydro_lupoalberto98/analysis_loss.py
+++ src/MultiBasinHydro_lupoalberto98/analysis_loss.py
@@ -88,10 +88,10 @@ if __name__ == '__main__':
     ### find the best models
     idx_ae = np.argmax(ae_nse)[0]
     epoch_max_nse = ae_nse[idx_ae]
-    print("Best LSTM-AE (27 features) model attained at epoch %d"%epoch_max_nse)
+    print("Best LSTM-AE (27 features) model obtained at epoch %d"%epoch_max_nse)
     idx_lstm = np.argmax(lstm_nse)[0]
     epoch_max_nse = epochs_lstm[idx_lstm]
-    print("Best LSTM model attained at epoch %d"%epoch_max_nse)
+    print("Best LSTM model obtained at epoch %d"%epoch_max_nse)
     idx_ae5f = np.argmax(ae_nf5_nse)[0]
     epoch_max_nse = epochs_ae_nf5[idx_ae5f]
-    print("Best LSTM-AE (5 features) model attained at epoch %d"%epoch_max_nse)
+    print("Best LSTM-AE (5 features) model obtained at epoch %d"%epoch_max_nse)
