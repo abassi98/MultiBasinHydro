4adf13e3de220db6b490ccb8b0a160dc2aa18587
lupoalberto98 <alberto.bassi98@gmail.com>
2023-02-13 13:30:15 +0100

    up

--- gpu_LSTM_AE.sh
+++ gpu_LSTM_AE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=gpu_LSTM_AE       
+#SBATCH --job-name=gpu_LSTM_AE_bidirectional       
 #SBATCH --time=24:00:00 
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=24
@@ -8,8 +8,8 @@
 #SBATCH --account=em09      
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END,FAIL     
-#SBATCH --output=gpu_LSTM_AE.out
-#SBATCH --error=gpu_LSTM_AE.err
+#SBATCH --output=gpu_LSTM_AE_bidirectional.out
+#SBATCH --error=gpu_LSTM_AE_bidirectional.err
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 module load daint-gpu PyTorch     
