d565fa2cafe68c53728bf620e41120c41e090ada
lupoalberto98 <alberto.bassi98@gmail.com>
2023-02-14 17:07:20 +0100

    Up

--- gpu_LSTM_AE.sh
+++ gpu_LSTM_AE.sh
@@ -13,4 +13,4 @@
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 module load daint-gpu PyTorch     
-python3 src/MultiBasinHydro_lupoalberto98/LSTM_AE_main.py --bidirectional True       
+python3 src/MultiBasinHydro_lupoalberto98/LSTM_AE_main.py --num_features 5 --bidirectional True       
