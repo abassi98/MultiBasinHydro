415ab5211ec0cf16d44f2bf71ed510b1c3674890
Alberto Bassi <alberto.bassi.3@studenti.unipd.it>
2023-02-15 11:48:11 +0100

    cleaned a little analys_loss

--- src/MultiBasinHydro_lupoalberto98/analysis_loss.py
+++ src/MultiBasinHydro_lupoalberto98/analysis_loss.py
@@ -24,28 +24,8 @@ from utils import Scale_Data, MetricsCallback, NSELoss
 
 
 
-##########################################################
-# set seed
-##########################################################
-torch.manual_seed(42)
-
-
 if __name__ == '__main__':
 
-    ##########################################################
-    # dataset 
-    ##########################################################
-    # Dataset
-    """
-    dates = ["1989/10/01", "2009/09/30"]
-    camel_dataset = CamelDataset(dates)
-    #dataset.adjust_dates() # adjust dates if necessary
-    camel_dataset.load_data() # load data
-    num_basins = camel_dataset.__len__()
-    seq_len = camel_dataset.seq_len
-    print("Number of basins: %d" %num_basins)
-    print("Number of points: %d" %seq_len)
-    """
     # data files
     data_ae = glob.glob("checkpoints/lstm-ae/*.ckpt")
     data_lstm = glob.glob("checkpoints/lstm/*.ckpt")
