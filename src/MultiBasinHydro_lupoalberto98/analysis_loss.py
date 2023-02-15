d4e864137246bd9ad9a035505334b9f5a21e5314
Alberto Bassi <alberto.bassi.3@studenti.unipd.it>
2023-02-15 12:40:33 +0100

    clean data

--- src/MultiBasinHydro_lupoalberto98/analysis_loss.py
+++ src/MultiBasinHydro_lupoalberto98/analysis_loss.py
@@ -45,70 +45,76 @@ if __name__ == '__main__':
     epochs_bidir = []
     epochs_bdir_nf5 = []
 
+    dict_ae = {}
+
     for file in data_ae:
         epoch = re.findall(r'\b\d+\b', file)
         epochs_ae.append(int(epoch[0]))
         checkpoint_ae = torch.load(file, map_location=lambda storage, loc: storage)
-        ae_nse.append(-checkpoint_ae["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
+        #ae_nse.append(-checkpoint_ae["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
+        val_loss = checkpoint_ae["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item()
+        epoch = checkpoint_ae["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["epoch"].item()
+        dict_ae["Epoch: "+str(epoch)] = {"val_loss" : val_loss}
+
+    torch.save(dict_ae, "checkpoints/lstm-ae/metrics.pt")
     
-  
-    for file in data_lstm:
-        epoch = re.findall(r'\b\d+\b', file)
-        epochs_lstm.append(int(epoch[0]))
-        checkpoint_ae = torch.load(file, map_location=lambda storage, loc: storage)
-        lstm_nse.append(-checkpoint_ae["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
+    # for file in data_lstm:
+    #     epoch = re.findall(r'\b\d+\b', file)
+    #     epochs_lstm.append(int(epoch[0]))
+    #     checkpoint_ae = torch.load(file, map_location=lambda storage, loc: storage)
+    #     lstm_nse.append(-checkpoint_ae["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
         
-    for file in data_lstm_noise:
-        epoch = re.findall(r'\b\d+\b', file)
-        epochs_lstm_noise.append(int(epoch[0]))
-        checkpoint = torch.load(file, map_location=lambda storage, loc: storage)
-        lstm_noise_nse.append(-checkpoint["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
+    # for file in data_lstm_noise:
+    #     epoch = re.findall(r'\b\d+\b', file)
+    #     epochs_lstm_noise.append(int(epoch[0]))
+    #     checkpoint = torch.load(file, map_location=lambda storage, loc: storage)
+    #     lstm_noise_nse.append(-checkpoint["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
         
-    for file in data_bidir:
-        epoch = re.findall(r'\b\d+\b', file)
-        epochs_bidir.append(int(epoch[0]))
-        checkpoint = torch.load(file, map_location=lambda storage, loc: storage)
-        bidir_nse.append(-checkpoint["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
+    # for file in data_bidir:
+    #     epoch = re.findall(r'\b\d+\b', file)
+    #     epochs_bidir.append(int(epoch[0]))
+    #     checkpoint = torch.load(file, map_location=lambda storage, loc: storage)
+    #     bidir_nse.append(-checkpoint["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
         
-    for file in data_bdir_nf5:
-        epoch = re.findall(r'\b\d+\b', file)
-        epochs_bdir_nf5.append(int(epoch[0]))
-        checkpoint = torch.load(file, map_location=lambda storage, loc: storage)
-        bdir_nf5_nse.append(-checkpoint["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
+    # for file in data_bdir_nf5:
+    #     epoch = re.findall(r'\b\d+\b', file)
+    #     epochs_bdir_nf5.append(int(epoch[0]))
+    #     checkpoint = torch.load(file, map_location=lambda storage, loc: storage)
+    #     bdir_nf5_nse.append(-checkpoint["callbacks"]["ModelCheckpoint{'monitor': 'val_loss', 'mode': 'min', 'every_n_train_steps': 0, 'every_n_epochs': 1, 'train_time_interval': None}"]["current_score"].item())
         
 
 
-    fig2, ax2 = plt.subplots(1,1,figsize=(10,10))
-    epochs_ae, ae_nse = zip(*sorted(zip(epochs_ae, ae_nse)))
-    epochs_lstm, lstm_nse = zip(*sorted(zip(epochs_lstm, lstm_nse)))
-    epochs_lstm_noise, lstm_noise_nse = zip(*sorted(zip(epochs_lstm_noise, lstm_noise_nse)))
-    epochs_bidir, bidir_nse = zip(*sorted(zip(epochs_bidir, bidir_nse)))
-    epochs_bdir_nf5, bdir_nf5_nse = zip(*sorted(zip(epochs_bdir_nf5, bdir_nf5_nse)))
+    # fig2, ax2 = plt.subplots(1,1,figsize=(10,10))
+    # epochs_ae, ae_nse = zip(*sorted(zip(epochs_ae, ae_nse)))
+    # epochs_lstm, lstm_nse = zip(*sorted(zip(epochs_lstm, lstm_nse)))
+    # epochs_lstm_noise, lstm_noise_nse = zip(*sorted(zip(epochs_lstm_noise, lstm_noise_nse)))
+    # epochs_bidir, bidir_nse = zip(*sorted(zip(epochs_bidir, bidir_nse)))
+    # epochs_bdir_nf5, bdir_nf5_nse = zip(*sorted(zip(epochs_bdir_nf5, bdir_nf5_nse)))
 
-    ax2.plot(epochs_ae,ae_nse, label="LSTM-AE 27 Features")
-    ax2.plot(epochs_lstm,lstm_nse, label="LSTM")
-    ax2.plot(epochs_lstm_noise,lstm_noise_nse, label="LSTM + 27 Noise")
-    ax2.plot(epochs_bidir,bidir_nse, label="LSTM Bidirectional AE (27 Features)")
-    ax2.plot(epochs_bdir_nf5,bdir_nf5_nse, label="LSTM Bidirectional AE (5 Features)")
+    # ax2.plot(epochs_ae,ae_nse, label="LSTM-AE 27 Features")
+    # ax2.plot(epochs_lstm,lstm_nse, label="LSTM")
+    # ax2.plot(epochs_lstm_noise,lstm_noise_nse, label="LSTM + 27 Noise")
+    # ax2.plot(epochs_bidir,bidir_nse, label="LSTM Bidirectional AE (27 Features)")
+    # ax2.plot(epochs_bdir_nf5,bdir_nf5_nse, label="LSTM Bidirectional AE (5 Features)")
 
-    ax2.set_xlabel("epoch")
-    ax2.set_ylabel("NSE")
-    ax2.legend()
-    fig2.savefig("hydro-lstm-ae_NSE.png")
+    # ax2.set_xlabel("epoch")
+    # ax2.set_ylabel("NSE")
+    # ax2.legend()
+    # fig2.savefig("hydro-lstm-ae_NSE.png")
     
-    ### find the best models
-    idx_ae = np.argmax(ae_nse)
-    epoch_max_nse = epochs_ae[idx_ae]
-    print("Best LSTM-AE (27 features) model obtained at epoch %d"%epoch_max_nse)
-    idx_lstm = np.argmax(lstm_nse)
-    epoch_max_nse = epochs_lstm[idx_lstm]
-    print("Best LSTM model obtained at epoch %d"%epoch_max_nse)
-    idx_noise = np.argmax(lstm_noise_nse)
-    epoch_max_nse = epochs_lstm_noise[idx_noise]
-    print("Best LSTM + Noise (27 features) model obtained at epoch %d"%epoch_max_nse)
-    idx_noise = np.argmax(bidir_nse)
-    epoch_max_nse = epochs_bidir[idx_noise]
-    print("Best LSTM-AE-BIDIR (27 features) model obtained at epoch %d"%epoch_max_nse)
-    idx_noise = np.argmax(bdir_nf5_nse)
-    epoch_max_nse = epochs_bdir_nf5[idx_noise]
-    print("Best LSTM-AE-BIDIR (5 features) model obtained at epoch %d"%epoch_max_nse)
+    # ### find the best models
+    # idx_ae = np.argmax(ae_nse)
+    # epoch_max_nse = epochs_ae[idx_ae]
+    # print("Best LSTM-AE (27 features) model obtained at epoch %d"%epoch_max_nse)
+    # idx_lstm = np.argmax(lstm_nse)
+    # epoch_max_nse = epochs_lstm[idx_lstm]
+    # print("Best LSTM model obtained at epoch %d"%epoch_max_nse)
+    # idx_noise = np.argmax(lstm_noise_nse)
+    # epoch_max_nse = epochs_lstm_noise[idx_noise]
+    # print("Best LSTM + Noise (27 features) model obtained at epoch %d"%epoch_max_nse)
+    # idx_noise = np.argmax(bidir_nse)
+    # epoch_max_nse = epochs_bidir[idx_noise]
+    # print("Best LSTM-AE-BIDIR (27 features) model obtained at epoch %d"%epoch_max_nse)
+    # idx_noise = np.argmax(bdir_nf5_nse)
+    # epoch_max_nse = epochs_bdir_nf5[idx_noise]
+    # print("Best LSTM-AE-BIDIR (5 features) model obtained at epoch %d"%epoch_max_nse)
