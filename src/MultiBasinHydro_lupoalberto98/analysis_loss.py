83840e3ebe2c0029860794a51b2777a26f8e5bdc
lupoalberto98 <alberto.bassi98@gmail.com>
2023-02-16 14:25:38 +0100

    Up

--- src/MultiBasinHydro_lupoalberto98/analysis_loss.py
+++ src/MultiBasinHydro_lupoalberto98/analysis_loss.py
@@ -29,7 +29,6 @@ if __name__ == '__main__':
 
     #####################################################################
     data = glob.glob("checkpoints/lstm-ae/*.ckpt")
-    print(data)
     data.remove("checkpoints/lstm-ae/last.ckpt")
     epochs = []
     nse = []
