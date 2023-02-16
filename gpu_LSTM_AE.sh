853f6fd85587d173f8b3c7edb87646a202d3b654
lupoalberto98 <alberto.bassi98@gmail.com>
2023-02-16 13:41:51 +0100

    4 hidden space features

--- gpu_LSTM_AE.sh
+++ gpu_LSTM_AE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=gpu_LSTM_AE_bidirectional27     
+#SBATCH --job-name=gpu_LSTM_AE_bidirectional4    
 #SBATCH --time=24:00:00 
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=24
@@ -8,9 +8,9 @@
 #SBATCH --account=em09      
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END,FAIL     
-#SBATCH --output=gpu_LSTM_AE_bidirectional27.out
-#SBATCH --error=gpu_LSTM_AE_bidirectional27.err
+#SBATCH --output=gpu_LSTM_AE_bidirectional4.out
+#SBATCH --error=gpu_LSTM_AE_bidirectional4.err
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 module load daint-gpu PyTorch     
-python3 src/MultiBasinHydro_lupoalberto98/LSTM_AE_main.py --num_features 27 --bidirectional 1 --debug 0 # bidirectional, training mode
+python3 src/MultiBasinHydro_lupoalberto98/LSTM_AE_main.py --num_features 4 --bidirectional 1 --debug 0 # bidirectional, training mode
