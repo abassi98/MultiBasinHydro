fc81ff0e5e226f0d5a5c5a751318adffdcf4db3a
Alberto Bassi <alberto.bassi.3@studenti.unipd.it>
2023-02-17 12:39:37 +0100

    up

--- gpu_LSTM_AE.sh
+++ gpu_LSTM_AE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=gpu_LSTM_AE_bdFalse4    
+#SBATCH --job-name=gpu_LSTM_AE_bdTrue4    
 #SBATCH --time=24:00:00 
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=24
@@ -8,9 +8,9 @@
 #SBATCH --account=em09      
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END,FAIL     
-#SBATCH --output=gpu_LSTM_AE_bdFalse4.out
-#SBATCH --error=gpu_LSTM_AE_bdFalse4.err
+#SBATCH --output=gpu_LSTM_AE_bdTrue4.out
+#SBATCH --error=gpu_LSTM_AE_bdTrue4.err
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 module load daint-gpu PyTorch     
-python3 src/MultiBasinHydro_lupoalberto98/LSTM_AE_main.py --num_features 4 --bidirectional 0 --debug 0 # no bidirectional, training mode
+python3 src/MultiBasinHydro_lupoalberto98/LSTM_AE_main.py --num_features 4 --bidirectional 1 --debug 0 # no bidirectional, training mode
