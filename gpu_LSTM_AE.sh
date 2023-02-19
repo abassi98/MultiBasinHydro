de9a5e766dd527b5bd9487eb36c2279e7bc006b6
Alberto Bassi <alberto.bassi.3@studenti.unipd.it>
2023-02-20 00:10:14 +0100

    up

--- gpu_LSTM_AE.sh
+++ gpu_LSTM_AE.sh
@@ -13,4 +13,4 @@
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 module load daint-gpu PyTorch     
-python3 src/MultiBasinHydro_lupoalberto98/LSTM_AE_main.py --num_features 27 --bidirectional 0 --debug 0 # no bidirectional, training mode
+python3 src/MultiBasinHydro_lupoalberto98/LSTM_AE_main.py --num_features 27 --bidirectional 1 --debug 0 # no bidirectional, training mode
