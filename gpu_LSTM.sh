fb9c2e4087dbbbdbed9441b913d0675d41c1fb83
Alberto Bassi <alberto.bassi.3@studenti.unipd.it>
2023-03-24 09:45:40 +0100

    up

--- gpu_LSTM.sh
+++ gpu_LSTM.sh
@@ -5,13 +5,12 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=24
 #SBATCH --constraint=gpu
-#SBATCH --partition=earth-4
-#SBATCH --account=xbs4  
+#SBATCH --account=em09
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END,FAIL     
 #SBATCH --output=gpu_LSTM-bdTrueStat.out
 #SBATCH --error=gpu_LSTM-bdTrueStat.err
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-module load python/3.9.12-pe5.34 #module load daint-gpu PyTorch      
+module load daint-gpu PyTorch      
 python3 src/LSTM_main.py --noise_dim 0 --statics 1 --bidirectional 1 --debug 0 # no noise, static features addes, bidirectional, training mode
