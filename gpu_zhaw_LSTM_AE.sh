5b9df5d39397eb8a03465e25558b8ac2a6afc0cd
Alberto Bassi <alberto.bassi.3@studenti.unipd.it>
2023-04-01 20:39:43 +0200

    Up

--- gpu_zhaw_LSTM_AE.sh
+++ gpu_zhaw_LSTM_AE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=gpu_LSTM_AE_bdTrue30Y
+#SBATCH --job-name=gpu_LSTM_AE_bdTrue30_1Y
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -10,8 +10,8 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END,FAIL     
-#SBATCH --output=gpu_LSTM_AE-bdTrue30Y.out
-#SBATCH --error=gpu_LSTM_AE-bdTrue30Y.err
+#SBATCH --output=gpu_LSTM_AE-bdTrue30_1Y.out
+#SBATCH --error=gpu_LSTM_AE-bdTrue30_1Y.err
 
 module load gcc/9.4.0-pe5.34 miniconda3/4.12.0 lsfm-init-miniconda/1.0.0	
 conda activate my_env
