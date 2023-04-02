de54afbc493ecc0f3b6c6f6df660caf557cec284
Alberto Bassi <alberto.bassi.3@studenti.unipd.it>
2023-04-02 12:07:55 +0200

    Up

--- gpu_zhaw_LSTM.sh
+++ gpu_zhaw_LSTM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=gpu_LSTM_StatHydro
+#SBATCH --job-name=gpu_LSTM
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -10,9 +10,9 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END,FAIL     
-#SBATCH --output=gpu_LSTM-bdTrueStatHydro.out
-#SBATCH --error=gpu_LSTM-bdTrueStatHydro.err
+#SBATCH --output=gpu_LSTM-bdTrue.out
+#SBATCH --error=gpu_LSTM-bdTrue.err
 
 module load gcc/9.4.0-pe5.34 miniconda3/4.12.0 lsfm-init-miniconda/1.0.0	
 conda activate my_env
-python3 src/LSTM_main.py --noise_dim 0 --statics 1 --hydro 1 --debug 0 
+python3 src/LSTM_main.py --noise_dim 0 --statics 0 --hydro 0 --debug 0 
